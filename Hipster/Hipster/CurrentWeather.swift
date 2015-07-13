9c4117d2ae594b23c90501c8b9852a029e4d4a06
Dulio Denis <dulio.denis@yahoo.com>
2015-07-13 16:31:36 -0400

    Refactored CurrentWeather struct with all optionals

--- Hipster/Hipster/CurrentWeather.swift
+++ Hipster/Hipster/CurrentWeather.swift
@@ -10,18 +10,27 @@ import Foundation
 
 struct CurrentWeather {
     
-    let temperature: Int
-    let humidity: Int
-    let precipProbability: Int
-    let summary: String
+    let temperature: Int?
+    let humidity: Int?
+    let precipProbability: Int?
+    let summary: String?
     
     init(weatherDictionary: [String: AnyObject]) {
-        temperature = weatherDictionary["temperature"] as! Int
-        let humidityFloat = weatherDictionary["humidity"] as! Double
-        humidity = Int(humidityFloat * 100)
-        let precipitationProbabilityFloat = weatherDictionary["precipProbability"] as! Double
-        precipProbability = Int(precipitationProbabilityFloat * 100)
-        summary = weatherDictionary["summary"] as! String
+        temperature = weatherDictionary["temperature"] as? Int
+        
+        if let humidityFloat = weatherDictionary["humidity"] as? Double {
+            humidity = Int(humidityFloat * 100)
+        } else {
+            humidity = nil
+        }
+        
+        if let precipitationProbabilityFloat = weatherDictionary["precipProbability"] as? Double {
+            precipProbability = Int(precipitationProbabilityFloat * 100)
+        } else {
+            precipProbability = nil
+        }
+        
+        summary = weatherDictionary["summary"] as? String
     }
     
 }
