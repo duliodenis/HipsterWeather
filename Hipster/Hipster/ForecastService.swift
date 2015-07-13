cf06b12fd38e8020eb8c0def2847cc935580fba8
Dulio Denis <dulio.denis@yahoo.com>
2015-07-13 15:51:38 -0400

    Added ForecastService struct

---
+++ Hipster/Hipster/ForecastService.swift
@@ -0,0 +1,43 @@
+//
+//  ForecastService.swift
+//  Hipster
+//
+//  Created by Dulio Denis on 7/13/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+import Foundation
+
+struct ForecastService {
+    let forecastAPIKey: String
+    
+    let forecastBaseURL: NSURL?
+    
+    init(APIKey: String) {
+        forecastAPIKey = APIKey
+        forecastBaseURL = NSURL(string: "http://api.forecast.io/\(forecastAPIKey)/")
+    }
+    
+    func getForecast(latitude: Double, longitude: Double, completion: (CurrentWeather? -> Void)) {
+        if let forecastURL = NSURL(string: "\(latitude),\(longitude)", relativeToURL: forecastBaseURL) {
+            let networkOperation = NetworkOperation(url: forecastURL)
+            networkOperation.downloadJSONFromURL {
+                (let JSONDictionary) in
+                let currentWeather = self.currentWeatherFromJSON(JSONDictionary)
+                completion(currentWeather)
+            }
+        } else {
+            println("Could not construct a valid URL")
+        }
+        
+    }
+    
+    func currentWeatherFromJSON(jsonDictionary: [String: AnyObject]?) -> CurrentWeather? {
+        if let currentWeatherDictionary = jsonDictionary?["currently"] as? [String: AnyObject] {
+            return CurrentWeather(weatherDictionary: currentWeatherDictionary)
+        } else {
+            println("JSON Dictionary returned nil for 'currently' key")
+            return nil
+        }
+    }
+}
